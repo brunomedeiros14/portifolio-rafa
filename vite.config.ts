f2eb22da1675eacd7ac837c62ef3116b2ffcc870
Bruno Medeiros <100999610+brunomedeiros14@users.noreply.github.com>
2026-09-03 14:22:12 -0300

    feat: update images and simplify build script in project configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    root: '.',
-    publicDir: 'assets',
-    build: {
-        outDir: 'dist',
-        emptyOutDir: true,
-        target: 'es2020',
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules/three')) {
-                        return 'three';
-                    }
-                },
-            },
+  root: ".",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    target: "es2020",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/three")) {
+            return "three";
+          }
         },
+      },
     },
-    server: {
-        port: 3000,
-        open: true,
-    },
-    css: {
-        devSourcemap: true,
-    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  css: {
+    devSourcemap: true,
+  },
 });
